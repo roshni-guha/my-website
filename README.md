81b53a909f59250f050c3958f9ff6ec22db2557b
roshni-guha <rguha9@wisc.edu>
2025-10-14 10:35:14 -0500

    Created class

---
+++ README.md
@@ -0,0 +1,5 @@
+**LEARNING POINTS**
+
+- React connects to HTML via a container with createRoot() function
+- Class is a type of function and we give it a contructor() method
+ 
--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,11 @@ import ReactDOM from 'react-dom/client';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <h1>Hello React!</h1>
 );
+
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+}
+
+const mycar = new Car("Ford");
