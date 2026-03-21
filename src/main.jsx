4ba2a7b087fd05ed6ddeb0c0f9fb603d36335a17
roshni-guha <rguha9@wisc.edu>
2026-03-21 12:38:15 -0500

    Added analytics to this

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import './index.css';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './home.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <Home />
+  <>
+    <Home />
+    <Analytics />
+  </>
 );
